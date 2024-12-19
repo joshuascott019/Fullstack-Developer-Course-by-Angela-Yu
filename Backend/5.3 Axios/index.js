b5bba2b44555f8d7c66dbc588ed87a77f710a069
Joshua Scott <joshua.ds.s019@gmail.com>
2024-12-19 10:04:03 -0300

    completed 5.3 Axios

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -1,31 +1,48 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const response = await axios.get('https://bored-api.appbrewery.com/random');
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    res.render('index.ejs', { data: result });
   } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    console.error('Failed to make request:', error.message);
+    res.render('index.ejs', {
       error: error.message,
     });
   }
 });
 
-app.post("/", async (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  try {
+    console.log(req.body);
+    const type = req.body.type;
+    const participants = req.body.participants;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+    );
+    const result = response.data;
+    console.log(result);
+    res.render('index.ejs', {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error('Failed to make request', error.message);
+    res.render('index.ejs', {
+      error: 'No activities match credentials.',
+    });
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
