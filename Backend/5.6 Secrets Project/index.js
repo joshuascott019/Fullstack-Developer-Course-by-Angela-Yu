2a6c780e05d5b565cf9cc23f616b9817c6e1755a
Joshua Scott <joshua.ds.s019@gmail.com>
2024-12-20 11:36:58 -0300

    complete 5.6 Secrets Project

--- Backend/5.6 Secrets Project/index.js
+++ Backend/5.6 Secrets Project/index.js
@@ -1,8 +1,4 @@
 // HINTS:
-// 1. Import express and axios
-
-// 2. Create an express app and set the port number.
-
 // 3. Use the public folder for static files.
 
 // 4. When the user goes to the home page it should render the index.ejs file.
@@ -11,3 +7,29 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const PORT = 3000;
+const API_URL = 'https://secrets-api.appbrewery.com/random';
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    res.render('index.ejs', {
+      secret: response.data.secret,
+      user: response.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
