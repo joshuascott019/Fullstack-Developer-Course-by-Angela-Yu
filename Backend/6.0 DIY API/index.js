b70fe5a6468e32b8cf01ce7cf0ad0d7876f4c8d8
Joshua Scott <joshua.ds.s019@gmail.com>
2025-04-21 14:00:56 -0300

    6.0 DIY API - implement get specific joke

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -14,8 +14,18 @@ app.get('/random', (req, res) => {
 });
 
 //2. GET a specific joke
+app.get('/jokes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
+app.get('/filter', (req, res) => {
+  const type = req.query.type;
+  const filteredJokes = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredJokes);
+});
 
 //4. POST a new joke
 
